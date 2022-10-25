3c4aabc7c2b4049c06399b35fd28e04ef95fea33
JessicaPersou <jessica.sousa@koin.com.br>
2022-10-25 11:45:29 -0300

    terminar formik e yup

--- coffee-react/src/components/Cadastro/Index.jsx
+++ coffee-react/src/components/Cadastro/Index.jsx
@@ -1,5 +1,5 @@
 import "./cadastro.css";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
@@ -14,17 +14,17 @@ const schema = yup.object().shape({
   numero: yup.number().max(5).positive().integer(),
   bairro: yup.string(),
   cidade: yup.string(),
-  estado: yup.string()
+  estado: yup.string(),
 });
 
 export function CadastroUsuario() {
   return (
-    <div className="form-register">
+    <div>
       <h2>
         ["<span className="detalhe-produtos">cadastre-se</span>"];
       </h2>
       <Formik
-      validationSchema={schema}
+        validationSchema={schema}
         initialValues={{
           nome: "",
           documento: "",
@@ -40,78 +40,61 @@ export function CadastroUsuario() {
           estado: "",
         }}
       >
-        {(ErrorMessage) => (
+        {() => (
           <Form>
             <div>
               <label htmlFor="nome">Nome</label>
-              <Field id="nome" name="nome" className="main" type="text" />
+              <Field id="nome" name="nome" type="text" />
             </div>
             <div>
               <label htmlFor="documento">Documento</label>
-              <Field
-                id="documento"
-                name="documento"
-                className="main"
-                type="number"
-              />
+              <Field id="documento" name="documento" type="number" />
             </div>
             <div>
               <label htmlFor="idade">Idade</label>
-              <Field id="idade" name="idade" className="main" type="number" />
+              <Field id="idade" name="idade" type="number" />
             </div>
             <div>
               <label htmlFor="telefone">Telefone</label>
-              <Field
-                id="telefone"
-                name="telefone"
-                className="main"
-                type="text"
-              />
+              <Field id="telefone" name="telefone" type="text" />
             </div>
             <div>
               <label htmlFor="email">E-mail</label>
-              <Field id="email" name="email" className="main" type="email" />
+              <Field id="email" name="email" type="email" />
             </div>
             <div>
               <label htmlFor="senha">Senha</label>
-              <Field id="senha" name="senha" className="main" type="text" />
+              <Field id="senha" name="senha" type="text" />
             </div>
             <div>
               <label htmlFor="cep">CEP</label>
-              <Field id="cep" name="cep" className="main" type="number" />
+              <Field id="cep" name="cep" type="number" />
             </div>
             <div>
               <label htmlFor="logradouro">Logradouro</label>
-              <Field
-                id="logradouro"
-                name="logradouro"
-                className="main"
-                type="text"
-              />
+              <Field id="logradouro" name="logradouro" type="text" />
             </div>
             <div>
               <label htmlFor="numero">Número</label>
-              <Field id="numero" name="numero" className="main" type="number" />
+              <Field id="numero" name="numero" type="number" />
             </div>
             <div>
               <label htmlFor="bairro">Bairro</label>
-              <Field id="bairro" name="bairro" className="main" type="text" />
+              <Field id="bairro" name="bairro" type="text" />
             </div>
             <div>
               <label htmlFor="cidade">Cidade</label>
-              <Field id="cidade" name="cidade" className="main" type="text" />
+              <Field id="cidade" name="cidade" type="text" />
             </div>
             <div>
               <label htmlFor="estado">Estado</label>
-              <Field id="estado" name="estado" className="main" type="text" />
+              <Field id="estado" name="estado" type="text" />
             </div>
           </Form>
         )}
-        <div className="send">
-          <button className="btn-card-produtos btn-send" type="submit">
-            cadastrar
-          </button>
-          <span className="cadastro">
+        <div>
+          <button type="submit">cadastrar</button>
+          <span>
             Você já tem Login? <a href="/login">Entre Aqui. </a>
           </span>
         </div>
