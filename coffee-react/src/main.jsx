bc80bcc92872ff9276be66c3a759475c3d0cc6f5
JessicaPersou <jessica.sousa@koin.com.br>
2022-11-08 09:49:30 -0300

    authenticated com context

--- coffee-react/src/main.jsx
+++ coffee-react/src/main.jsx
@@ -7,17 +7,20 @@ import Produtos from "../src/Pages/Produto.jsx";
 import Usuario from "../src/Pages/Usuario";
 import "../src/styles/global.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login/cadastro" element={<Cadastro />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/produtos" element={<Produtos />} />
-        <Route path="/usuario" element={<Usuario />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <AuthProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/usuario" element={<Usuario />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </AuthProvider>
 );
