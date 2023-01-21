cb26f0882aa483c7ad3f1e89382716c5f9fc7310
JessicaPersou <jessica.sousa@koin.com.br>
2023-01-21 19:51:45 -0300

    nome do banco

--- coffee-api/database/config/config.js
+++ coffee-api/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": 1864,
-    "database": "database_development",
+    "database": "coffee",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
